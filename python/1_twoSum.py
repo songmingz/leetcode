04552993542210349138e94fd751a18d3706fba6
songmingz <mingxpc@gmail.com>
2021-05-24 17:54:41 +0900

    commt

--- python/1_twoSum.py
+++ python/1_twoSum.py
@@ -1,7 +1,9 @@
 #1264ms memory:14.8MB
 def twoSum(nums, target):
         for i,num in enumerate(nums):
+            #计算出另一个加数
             add = target - num
+            #判断此加数是否在列表里(需要排除num,因为不能重复使用相同元素)
             if add in nums[0:i]:
                 return [i, nums[0:i].index(add)]
             if add in nums[i + 1:len(nums)]:
