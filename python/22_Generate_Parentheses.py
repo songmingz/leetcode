db58306271110152170a80f39a2aa9d04daa5683
songmingz <mingxpc@gmail.com>
2021-05-26 20:09:44 +0900

    commit

---
+++ python/22_Generate_Parentheses.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    #递归有待理解
+    def generateParenthesis(self, n):
+        def generate(A = []):
+            if len(A) == 2*n:
+                if valid(A):
+                    ans.append("".join(A))
+            else:
+                A.append('(')
+                generate(A)
+                A.pop()
+                A.append(')')
+                generate(A)
+                A.pop()
+
+        def valid(A):
+            bal = 0
+            #若左右括号数量不相等,则不合法
+            for c in A:
+                if c == '(': bal += 1
+                else: bal -= 1
+                if bal < 0: return False
+            return bal == 0
+
+        ans = []
+        generate()
+        return ans
